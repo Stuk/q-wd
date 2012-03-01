650aaf802ffe07cf05e6cf3c7c63695e5c931e24
Stuart Knightley <stuartk@motorola.com>
2012-02-29 17:20:15 -0800

    Update package.json and tests

--- test/test.chrome.js
+++ test/test.chrome.js
@@ -1,15 +1,16 @@
 var webdriver = require("../lib/main")
 
 var browser = webdriver.remote();
-browser.init({browserName:"chrome"}, function() {
-  browser.get("http://www.jelly.io", function() {
-    browser.eval("window.location.href", function(a, o) {
-      console.log(o);
-      browser.get("http://www.seleniumhq.org", function() {
-        browser.quit(function() {
-          console.log("DONE");
-        })
-      })
-    })
-  })
-})
+browser.init({browserName:"chrome"}).then(function() {
+  return browser.get("http://www.jelly.io");
+}).then(function() {
+  return browser.eval("window.location.href");
+}).then(function(o) {
+  console.log(o);
+  return browser.get("http://www.seleniumhq.org");
+}).then(function() {
+  return browser.quit();
+}).then(function() {
+  console.log("DONE");
+});
+
--- test/test.ondemand.js
+++ test/test.ondemand.js
@@ -3,7 +3,7 @@ var webdriver = require("../lib/main")
 var browser = webdriver.remote("ondemand.saucelabs.com", 80, "username", "access-key");
 browser.init(function() {
   browser.get("http://www.jelly.io", function() {
-    browser.eval("window.location.href", function(a, o) {
+    browser.eval("window.location.href", function(ao) {
       console.log(o);
       browser.get("http://www.seleniumhq.org", function() {
         browser.quit(function() {
